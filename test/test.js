c0c4ffc5f377674ead55273cf366a5dc00ed9461
Todor Angelov <tondy@tondy.com>
2018-05-20 20:28:13 +0200

    v0.0.5

--- test/test.js
+++ test/test.js
@@ -1,11 +1,13 @@
 //
 "use strict";
 
-const Crypto = require('./Crypto.js');
+const Crypto = require('../lib/Crypto.js');
 
 const $crypto = new Crypto();
 
 const msg = 'Both shared secrets are BN instances';
+const $psw = '32bytes-67VC61jmV54rIYu1545x4TlY';
+const $iv = 'tondy';
 
 const key = $crypto.genKeyPair();
 
@@ -15,12 +17,17 @@ var key2 = $crypto.genKeyPair();
 var shared1 = $crypto.secret(key.skey,key2.pkey);
 var shared2 = $crypto.secret(key2.skey,key.pkey);
 
-console.log('Both shared secrets are BN instances');
+console.log('Shared secret:',shared1.toString(16) === shared2.toString(16));
 console.log(shared1.toString(16));
 console.log(shared2.toString(16));
 
 var sign = $crypto.sign(key.skey, msg);
-//console.log(sign);
+console.log('sign:',JSON.stringify(sign));
 //const a = Array.from(new Uint8Array(sign));
 //console.log(a);
-console.log($crypto.verify(key.pkey, msg,sign));
+console.log('verify:', $crypto.verify(key.pkey, msg, sign));
+
+const a = [];
+for (let i=0;i<10;i++) a.push(msg);
+const s = a.join('\n');
+console.log($crypto.decrypt($crypto.encrypt(s,$psw,$iv),$psw,$iv).toString());
